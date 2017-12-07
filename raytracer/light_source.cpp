8e71f5dde7d7f01f9b4c3ce3435a887fc0f093b5
Ming En Sheng <max.sheng@mail.utoronto.ca>
2017-12-06 21:36:44 -0500

    fix area light phong

--- raytracer/light_source.cpp
+++ raytracer/light_source.cpp
@@ -1,139 +1,66 @@
 /***********************************************************
-     Starter code for Assignment 3
+Starter code for Assignment 3
 
-     This code was originally written by Jack Wang for
-		    CSC418, SPRING 2005
+This code was originally written by Jack Wang for
+CSC418, SPRING 2005
 
-		implements light_source.h
+implements light_source.h
 
 ***********************************************************/
 
 #include <cmath>
 #include "light_source.h"
 
-void PointLight::shade( Ray3D& ray ) {
+Colour CalculatePhong(Ray3D& ray, Point3D& lightPos, Colour& ambient) {
+	//Assume everything is in world coordinate 
 
-	if (ray.intersection.mat->text_array != NULL) {
-		unsigned char* texture = ray.intersection.mat->text_array;
-		//normal of intersection point and sphere
-		Vector3D nor = ray.intersection.normal;
-		nor.normalize();
+	Point3D intersectPoint = ray.intersection.point;
+	Vector3D normalVector = ray.intersection.normal;
+	normalVector.normalize();
+	Vector3D viewVector = -ray.dir;
+	viewVector.normalize();
+	Vector3D light_vector = (lightPos - intersectPoint);
+	light_vector.normalize();
+	Vector3D difference = (light_vector + viewVector);
+	//calculate reflect
+	Vector3D reflect = light_vector - 2 * (light_vector.dot(normalVector))*normalVector;
+	reflect.normalize();
+	difference.normalize();
+	//double cos_specular = fmax(difference.dot(normalVector), 0.0);
+	double cos_specular = fmax(reflect.dot(-viewVector), 0.0);
+	//std::cout << cos_specular << "\n";
+	double cos_diffuse = fmax(light_vector.dot(normalVector), 0.0);
 
-		int witdth = 256;
-		int height = 256;
-		//double u = 0.5 + atan2(nor[1], nor[0]) / (2.0 * 3.141592653589793);
-		//double v = 0.5 - acos(nor[2]) / 3.141592653589793;
-		double u = atan2(nor[1], nor[0]) / (2.0*3.141592653589793) + 0.5;
-		double v = nor[2] * 0.5 + 0.5;
+	double ka = 1;
+	double kd = 1;
+	double ks = 1;
+	//double ks = 3 - ka - kd;
 
-		int i = (int)(u * witdth);//256 is image width
-		int j = (int)(v * height);
+	Colour current_phong_color = (ka*ray.intersection.mat->ambient + kd*cos_diffuse*ray.intersection.mat->diffuse + ks*pow(cos_specular, ray.intersection.mat->specular_exp)*ray.intersection.mat->specular);
+	current_phong_color[0] = current_phong_color[0] * (ambient[0]);
+	current_phong_color[1] = current_phong_color[1] * (ambient[1]);
+	current_phong_color[2] = current_phong_color[2] * (ambient[2]);
+	current_phong_color.clamp();
+	return current_phong_color;
+}
 
-		unsigned int r;
-		unsigned int g;
-		unsigned int b;
+void PointLight::shade(Ray3D& ray) {
+	//Assume everything is in world coordinate 
+	// TODO: implement this function to fill in values for ray.col 
+	// using phong shading.  Make sure your vectors are normalized, and
+	// clamp colour values to 1.0.
+	//
+	// It is assumed at this point that the intersection information in ray 
+	// is available.  So be sure that traverseScene() is called on the ray 
+	// before this function.  
 
-		if ((i * 150 + j) % 3 != 0){
-			i = i - (i * 150 + j) % 3;
-		}
-
-		r = texture[j * witdth *3 + i*3];
-		g = texture[j * witdth *3 + i*3 + 1];
-		b = texture[j * witdth *3 + i*3 + 2];
-
-		double rf = r / 255.0;
-		double gf = g / 255.0;
-		double bf = b / 255.0;
-
-
-
-		ray.col = Colour(rf, gf, bf);
-	}
-	else {
-		//Assume everything is in world coordinate 
-		Point3D lightPos = this->get_position();
-	
-		Point3D intersectPoint = ray.intersection.point;
-		Vector3D normalVector = ray.intersection.normal;
-		normalVector.normalize();
-		Vector3D viewVector = -ray.dir;
-		viewVector.normalize();
-		Vector3D light_vector = (lightPos - intersectPoint);
-		light_vector.normalize();
-		Vector3D difference = (light_vector + viewVector);
-		//calculate reflect
-		Vector3D reflect = light_vector - 2*(light_vector.dot(normalVector))*normalVector;
-		reflect.normalize();
-		difference.normalize();
-		//double cos_specular = fmax(difference.dot(normalVector), 0.0);
-		double cos_specular = fmax(reflect.dot(-viewVector), 0.0);
-		//std::cout << cos_specular << "\n";
-		double cos_diffuse = fmax(light_vector.dot(normalVector), 0.0);
-
-		double ka = 1;
-		double kd = 1;
-		double ks = 1;
-		//double ks = 3 - ka - kd;
-
-		Colour current_phong_color = (ka*ray.intersection.mat->ambient + kd*cos_diffuse*ray.intersection.mat->diffuse + ks*pow(cos_specular, ray.intersection.mat->specular_exp)*ray.intersection.mat->specular);
-		current_phong_color[0] = current_phong_color[0]*(this->_col_ambient[0]);
-		current_phong_color[1] = current_phong_color[1]*(this->_col_ambient[1]);
-		current_phong_color[2] = current_phong_color[2]*(this->_col_ambient[2]);
-	
-		//ray.col = 0.5*ray.col + 0.5*current_phong_color;
-		ray.col = current_phong_color;
-
-		//ray.col = ka*(0.8*ray.intersection.mat->ambient+0.2*this->_col_ambient) + kd*cos_diffuse*(0.8*ray.intersection.mat->diffuse+0.2*this->_col_diffuse) + ks*pow(cos_specular, ray.intersection.mat->specular_exp)*(0.8*ray.intersection.mat->specular+0.2*this->_col_specular);
-		//clamp color to 1
-		//std::cout << ray.col << "\n";
-		ray.col.clamp();
-
-		// if (ray.col[0] > 1) ray.col[0] = 1;
-		// if (ray.col[1] > 1) ray.col[1] = 1;	
-		// if (ray.col[2] > 1) ray.col[2] = 1;
-
-
-
-		//double r = (double)rand() / (RAND_MAX) + 1;
-		//double g = (double)rand() / (RAND_MAX)+1;
-		//double b = (double)rand() / (RAND_MAX)+1;
-
-		//ray.col = Colour(r, g, b);
-	
-
-		// TODO: implement this function to fill in values for ray.col 
-		// using phong shading.  Make sure your vectors are normalized, and
-		// clamp colour values to 1.0.
-	
-		//
-		// It is assumed at this point that the intersection information in ray 
-		// is available.  So be sure that traverseScene() is called on the ray 
-		// before this function.  
-	}
-
+	Point3D lightPos = this->get_position();
+	Colour ambient = this->get_ambient_light();
+	Colour phongColor = CalculatePhong(ray, lightPos, ambient);
+	ray.col = phongColor;
 
 }
 
-///////////sample
-//  //find the input light vector
-//  vec3 light_vector = normalize(lightPos - vertPos);
-//  //find the viewing_vector
-//  vec3 eye_vec = normalize(vec3(0,0,0) - vertPos);
-//  //find the reflect vector
-//  vec3 reflect_vec = normalize(reflect(-light_vector, normalInterp));
-//
-//  //calculate crossproduct of eye vector and input light vector
-//  vec3 difference = normalize(light_vector + eye_vec);
-//
-//  //calculate specular Color
-//  //I have implemented both way to find specular cos value
-//  //float cos_specular = max(dot(difference, normalInterp), 0.0);
-//  float cos_specular = max(dot(reflect_vec, eye_vec), 0.0);
-//
-//  //calculate diffuse Color
-//  float cos_diffuse = max(dot(light_vector, normalInterp), 0.0);
-//  //float cos_diffuse = max(dot(light_vector, normalInterp), 0.0);
-//
-//
-//
-//  gl_FragColor = vec4(Ka*ambientColor+Kd*diffuseColor*cos_diffuse+Ks*specularColor*(pow(cos_specular, shininessVal)), 1.0);
+void AreaLight::shade(Ray3D & ray)
+{
+}
