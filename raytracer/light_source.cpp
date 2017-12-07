7a02740ee3a2955e4308832b1cab6179f2c4b6c3
Ming En Sheng <max.sheng@mail.utoronto.ca>
2017-12-06 21:45:08 -0500

    merge texture

--- raytracer/light_source.cpp
+++ raytracer/light_source.cpp
@@ -44,22 +44,54 @@ Colour CalculatePhong(Ray3D& ray, Point3D& lightPos, Colour& ambient) {
 	return current_phong_color;
 }
 
-void PointLight::shade(Ray3D& ray) {
-	//Assume everything is in world coordinate 
-	// TODO: implement this function to fill in values for ray.col 
-	// using phong shading.  Make sure your vectors are normalized, and
-	// clamp colour values to 1.0.
-	//
-	// It is assumed at this point that the intersection information in ray 
-	// is available.  So be sure that traverseScene() is called on the ray 
-	// before this function.  
 
-	Point3D lightPos = this->get_position();
-	Colour ambient = this->get_ambient_light();
-	Colour phongColor = CalculatePhong(ray, lightPos, ambient);
-	ray.col = phongColor;
+
+// TODO: implement this function to fill in values for ray.col 
+// using phong shading.  Make sure your vectors are normalized, and
+// clamp colour values to 1.0.
+
+//
+// It is assumed at this point that the intersection information in ray 
+// is available.  So be sure that traverseScene() is called on the ray 
+// before this function.  
+void PointLight::shade(Ray3D& ray) {
+	if (ray.intersection.mat->text_array != NULL) {
+		unsigned char* texture = ray.intersection.mat->text_array;
+		//normal of intersection point and sphere
+		Vector3D nor = ray.intersection.normal;
+		nor.normalize();
+
+		double u = 0.5 + atan2(nor[2], nor[0]) / (2 * 3.141592653589793);
+		double v = 0.5 - asin(nor[1]) / 3.141592653589793;
+
+		int i = (int)(u * 256);//105 is image width
+		int j = (int)(v * 256);
+		unsigned int r;
+		unsigned int g;
+		unsigned int b;
+
+		r = texture[j * 256 + i];
+		g = texture[j * 256 + i + 1];
+		b = texture[j * 256 + i + 2];
+
+		double rf = r / 255.0;
+		double gf = g / 255.0;
+		double bf = b / 255.0;
+		ray.col = Colour(rf, gf, bf);
+	}
+	else {
+		//Assume everything is in world coordinate 
+		Point3D lightPos = this->get_position();
+		Colour ambient = this->get_ambient_light();
+		Colour phongColor = CalculatePhong(ray, lightPos, ambient);
+		ray.col = phongColor;
+	}
 
 }
+
+
+
+
 
 void AreaLight::shade(Ray3D & ray)
 {
